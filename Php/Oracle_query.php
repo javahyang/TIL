606f6ac75d97a50d2bcaf1ba358ba89a6f56654e
javahyang <javahyang@gmail.com>
2021-04-14 18:33:29 +0900

    배열의 교집합 : array_intersect

--- Php/Oracle_query.php
+++ Php/Oracle_query.php
@@ -4,6 +4,3 @@
 $rawDate = '2021-04-12';
 $insertDate = date('Y-m-d H:i:s', strtotime("{$rawDate} 00:00:00"));
 $query = "insert into TABLE_NAME (DATE_COLUMN_NAME) values(to_date('".$insertDate."', 'YYYY-MM-DD hh24:mi:ss'))";
-
-// 배열의 차집합(arrayA-arrayB)
-$result = array_diff($array_A, $array_B);
---
+++ Php/Php_function.pyp
@@ -0,0 +1,7 @@
+<?
+
+// 배열의 차집합(arrayA-arrayB)
+$result = array_diff($array_A, $array_B);
+
+// 배열의 교집합
+$result = array_intersect($array_A, $array_B);
