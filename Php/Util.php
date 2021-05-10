d8b6d5a7c4a5e3a8009c8a0a71e28a8741e6b764
javahyang <javahyang@gmail.com>
2021-05-10 09:58:46 +0900

    마지막 글자 잘라내기

--- Php/Util.php
+++ Php/Util.php
@@ -3,3 +3,8 @@
 function getFormatDate($date, $format = 'Y-m-d\TH:i:sO') {
   return date($format, strtotime($date));
 }
+
+function removeLastChar($str) {
+  // return substr($str, 0, -1);
+  return substr_replace($str, '', -1);
+}
