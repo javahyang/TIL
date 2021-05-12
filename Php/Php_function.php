38915824ed6a70aec064afe1b3354f8ea0b0e8c9
javahyang <javahyang@gmail.com>
2021-05-12 18:04:00 +0900

    php array_map에 람다(익명함수) 이용

--- Php/Php_function.php
+++ Php/Php_function.php
@@ -14,3 +14,10 @@ $dom_sxe = dom_import_simplexml($sxe);
 $dom_sxe = $dom->importNode($dom_sxe, true);
 $dom_sxe = $dom->appendChild($dom_sxe);
 $dom->saveXML();
+
+// php 람다(익명함수) 이용, 배열 각 항목값 수정해서 문자열 만들기
+$items = ['a', 'b', 'c', 'd'];
+$str = implode(', ', array_map(function ($item) {
+  return "'{$item}'";
+}, $items));
+// 출력: 'a', 'b', 'c', 'd'
