9924c7eb2c009305dd22518597fbfa1627c06aba
javahyang <javahyang@gmail.com>
2021-05-31 17:18:51 +0900

    문자열 치환하고 문자열을 날짜범위로 검색

--- Php/Oracle_query.php
+++ Php/Oracle_query.php
@@ -4,3 +4,8 @@
 $rawDate = '2021-04-12';
 $insertDate = date('Y-m-d H:i:s', strtotime("{$rawDate} 00:00:00"));
 $query = "insert into TABLE_NAME (DATE_COLUMN_NAME) values(to_date('".$insertDate."', 'YYYY-MM-DD hh24:mi:ss'))";
+
+
+// 문자열 치환하고 문자열을 날짜로 검색
+$rawDate = '2021/05/31';
+$query = "select date_column from TABLE_NAME where to_date(replace(date_column, '/', ''), 'yyyymmdd) between to_date('20210501', 'yyyymmdd') and to_date('20210505', 'yyyymmdd') order by replace(date_column, '/', '') desc";
